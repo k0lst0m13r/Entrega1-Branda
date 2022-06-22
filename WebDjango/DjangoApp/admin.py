eee75bb1a7eaae0b15dc74850752f9d68fc6b6bc
Julio Branda <tete@L4-P0lv3r1t4>
2022-06-22 16:11:08 -0300

    terminado modelo para entregable, arreglada pagina activa en navbar

--- WebDjango/DjangoApp/admin.py
+++ WebDjango/DjangoApp/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Comentarios
+from .models import *
 
 # Register your models here.
 admin.site.register(Comentarios)
+admin.site.register(Subscripcion)
+admin.site.register(Servicios)
--- WebDjango/DjangoApp/models.py
+++ WebDjango/DjangoApp/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
-class Comentarios(models.Model):
-    
+
+
+class Comentarios(models.Model):    
     nombre = models.CharField(max_length=15)
-    coment = models.TextField()
+    comentario = models.TextField()
+    creado = models.DateTimeField(auto_now_add=True)
+
+    
+class Subscripcion(models.Model):    
+    email = models.EmailField(max_length=40)
+    
+    
+class Servicios(models.Model):    
+    servicio = models.CharField(max_length=20) 
