4e95981b16ae8f30e791f6982a4983b66537937e
Julio Branda <tete@L4-P0lv3r1t4>
2022-06-28 14:09:10 -0300

    mejorada visualizacion de datos en seccion admin

--- WebDjango/DjangoApp/forms.py
+++ WebDjango/DjangoApp/forms.py
@@ -19,3 +19,17 @@ class ServiciosForm(forms.ModelForm):
     class Meta:
         model = Servicios
         fields = ('servicio',)        
+        
+        
+class CrearPost(forms.ModelForm):
+    
+    class Meta:
+        model = Post
+        fields = ('titulo', 'post', 'autor',)
+        
+        
+class ImageForm(forms.ModelForm):
+    
+    class Meta:
+        model = ImagenPost
+        fields = ('imagen', 'descripcion',)
--- WebDjango/DjangoApp/models.py
+++ WebDjango/DjangoApp/models.py
@@ -3,14 +3,37 @@ from django.db import models
 
 
 class Comentarios(models.Model):    
-    nombre = models.CharField(max_length=25)
+    nombre = models.CharField(max_length=40)
     comentario = models.TextField()
     creado = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.nombre, self.creado
 
     
 class Subscripcion(models.Model):    
     email = models.EmailField(max_length=40)
-    
+    def __str__(self):
+        return self.email
     
 class Servicios(models.Model):    
-    servicio = models.CharField(max_length=30) 
+    servicio = models.CharField(max_length=30)
+    def __str__(self):
+        return self.servicio 
+
+    
+class Post(models.Model):
+    titulo = models.CharField(max_length=40)
+    post = models.TextField()
+    autor = models.CharField(max_length=40)
+    fecha = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.titulo, self. autor, self.fecha
+    
+
+class ImagenPost(models.Model):
+    descripcion = models.CharField(max_length=40)
+    imagen = models.ImageField(upload_to = "images/")
+    def __str__(self):
+        return self.descripcion
+
